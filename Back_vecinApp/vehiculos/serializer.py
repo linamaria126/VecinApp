c1e05014184020b2658811b910af5a5ec2a353e2
Lina Maria Cardona <linamaria126@gmail.com>
2026-03-19 11:05:47 -0500

    Modify vehiculos and zonas_sociales serializers

--- Back_vecinApp/vehiculos/serializer.py
+++ Back_vecinApp/vehiculos/serializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Vehiculo
+from parqueaderos.models import Parqueadero
 
 class VehiculoSerializer(serializers.ModelSerializer):
+    parqueadero_info = serializers.SerializerMethodField()
+
     class Meta:
         model = Vehiculo
         fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+
+  
