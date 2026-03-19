5e3d014a56509e24b8f6dc2e243dad7d2efcc80f
Lina Maria Cardona <linamaria126@gmail.com>
2026-03-19 10:04:15 -0500

    Modify import get_user_model at publicaciones serializers

--- Back_vecinApp/publicaciones/serializer.py
+++ Back_vecinApp/publicaciones/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Publicacion
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
